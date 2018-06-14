5958cb8d81034a8d99f49237799ed2941ff2e668
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-06-14 15:17:08 -0400

    created test w/ fixture

---
+++ test/fixtures.js
@@ -0,0 +1,29 @@
+const stickers =[
+  {
+    id: 1,
+    title: "JavaScript",
+    description: "JS Logo",
+    rating: 10,
+    url: "https://devstickers.com/assets/img/cat/javascript.png"
+  },
+  {
+    id: 2,
+    title: "Angular 2",
+    description: "Angular 2",
+    rating: 10,
+    url: "https://devstickers.com/assets/img/cat/angular2.png"
+  },
+  {
+    id: 3,
+    title: "React.js",
+    description: "React",
+    rating: 10,
+    url: "https://devstickers.com/assets/img/cat/react-js.png"
+  }
+];
+
+
+
+module.exports = {
+  stickers
+}
